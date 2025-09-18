8009f1dc43c033212359cd966229d7d1d60ea7e2
Sang-Myeong Lee <teamjuji5@naver.com>
2025-09-18 17:00:29 +0900

    Initial commit:  Spring Boot project setup

---
+++ src/main/java/chungbuk/soabackend/SoaBackendApplication.java
@@ -0,0 +1,13 @@
+package chungbuk.soabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chungbuk/soabackend/SoaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package chungbuk.soabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
